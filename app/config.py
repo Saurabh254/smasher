71011c4f361cf0e23985553d9375a4ceda3ed194
Saurabh <sauravvishwakarma030@gmail.com>
2024-02-21 00:22:43 +0530

    Added message templete

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
 REDIS_HOST_NAME = "localhost"
 REDIS_PORT = 6379
 OTP_TIMEOUT = 60  # 1min
-MESSAGE_TEMPLATE = "OTP for app is {otp}"
+MESSAGE_TEMPLATE = "{otp} is your OTP for verification of phone number with FASTAPI. It is valid for 1 minutes only. - FastAPI"
